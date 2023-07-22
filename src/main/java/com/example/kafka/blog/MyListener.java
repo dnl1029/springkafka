71bf89e84e4a2502517504a7a8d873b5645d6674
hyungkyu.we <dnl1029@gmail.com>
2023-07-22 22:28:42 +0900

    MyListener 구현

---
+++ src/main/java/com/example/kafka/blog/MyListener.java
@@ -0,0 +1,31 @@
+package com.example.kafka.blog;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.kafka.listener.ConsumerAwareRebalanceListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+@Slf4j
+public class MyListener implements ConsumerAwareRebalanceListener {
+
+    //파티션 재할당시 호출(서버 처음 구동시에도 호출)
+    @Override
+    public void onPartitionsAssigned(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+        log.info("# PartitionsAssigned. metadata: {}, partitions: {}"
+                , consumer.groupMetadata()
+                , partitions);
+
+        partitions
+                .forEach(partition -> {
+                    // position에 나온 offset-1 이 현재까지 발행된 offset 수이며, position 값은 이제 들어올 예정인 offset
+                    log.info("partition: " + partition.partition() + ", position(readOffset): " + consumer.position(partition) + " , topic: " + partition.topic());
+                });
+    }
+}
